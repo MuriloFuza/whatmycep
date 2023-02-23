ba45c3313875bb87b412dc5a79f8d70b737deca3
MuriloFuza <muriloacademix@gmail.com>
2023-02-23 16:35:26 -0400

    remove node_modules and TS dependencies

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-const enderecoInput = document.getElementById("endereco") as HTMLInputElement;
-const botaoLimpar = document.querySelector("#limpar");
-const enviarButton = document.getElementById("enviar")!;
-
-enviarButton.addEventListener("click", () => {
-	const endereco = enderecoInput.value;
-	// Adicione aqui a lógica para enviar o endereço para algum lugar
-});
